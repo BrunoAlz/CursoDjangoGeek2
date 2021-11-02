bff43d5e7c966dd6c1da941bfa4c169e296bc9ec
Bruno Alves <Bruno6821@gmail.com>
2021-11-02 16:38:13 -0300

    Implementa Templates, Teste de Pagina 404

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 # Fazemos o import da CBV criado la em *core - views.py*
-from .views import IndexView
+from .views import ErrorView, IndexView
 
 urlpatterns = [
     #Caminho '' = RAIZ, IndexView é a Classe, e o nome 
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('error/', ErrorView.as_view(), name='error'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class ErrorView(TemplateView):
+    template_name = 'parciais/_404.html'
