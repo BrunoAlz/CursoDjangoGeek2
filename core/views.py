2c7019d8880e7f4bfda509820459a2e9889f0c38
Bruno Alves <Bruno6821@gmail.com>
2021-11-02 21:40:28 -0300

    Implementado dados do Banco nos serviços.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import TemplateView
+from .models import Categoria, Servico
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['categorias'] = Categoria.objects.all()
+        context['servicos'] = Servico.objects.all()
+        return context
+
 
 class ErrorView(TemplateView):
     template_name = 'parciais/_404.html'
